9c54e90e7905928090e5755e438b507591c48264
Francesco Di Sales <f.disales@hotmail.com>
2026-04-17 19:46:06 +0200

    Add pwd_dir variable and return to original directory

--- signal-proxy.sh
+++ signal-proxy.sh
@@ -1,6 +1,8 @@
 
+pwd_dir = $(pwd)
 
 git clone https://github.com/signalapp/Signal-TLS-Proxy.git
 mv nginx.conf Signal-TLS-Proxy/data/nginx-relay/
 cd Signal-TLS-Proxy
 bash init-certificate.sh
+cd $pwd_dir
